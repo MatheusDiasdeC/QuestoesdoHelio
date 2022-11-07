46bc1a44f8e1c75b3560a59f5cea4715e964e122
mat10 <mat10@DESKTOP-KJHDL3A>
2022-11-07 18:18:31 -0300

    Questão 5

---
+++ q5/src/main/java/q5/Conta.java
@@ -0,0 +1,30 @@
+package q5;
+
+public class Conta {
+	
+	public String titular;
+	public int numero;
+	public String agencia;
+	public double saldo;
+	public String dataAbertura;
+	
+	public void saca(int valor) {
+		this.saldo -= valor;
+	}
+	
+	public void deposita(int valor) {
+		this.saldo += valor;
+	}
+	
+	public double calculaRendimento() {
+		
+		double valor = saldo * 0.1;
+		return valor;
+	}
+
+	public void retornaDadosParaImpressao() {
+		System.out.println("Agencia: " + agencia + "\nTitular: " + titular + "\nNumero da Conta" + numero + "\nSaldo: " + saldo );
+	}
+	
+	public Conta() {};
+}
