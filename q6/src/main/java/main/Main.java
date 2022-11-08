d61a14d7787e61a6e1cf420e7e294bef10a16041
mat10 <mat10@DESKTOP-KJHDL3A>
2022-11-08 20:04:58 -0300

    Questão 6

---
+++ q6/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import q6.Conta;
+import q6.Data;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Conta c = new Conta();
+		c.retornaDadosParaImpressao();
+		Data d = new Data(7,8,9);
+		
+	}
+
+}
